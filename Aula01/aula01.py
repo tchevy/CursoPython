416f27a4069fd969b534199fe8a732e3cf644615
Roberto Nascimento <robertok3r@gmail.com>
2022-01-11 16:42:10 -0300

    Iniciando o Projeto Curso Python Avançado

---
+++ Aula01/aula01.py
@@ -0,0 +1,8 @@
+print("Digite se Nome:")
+nome = input()
+print("Digite sua idade:")
+idade = input()
+print("Digite sua escolariade:")
+esc = input()
+
+print("Hello " + nome,'!','sua Idade é: ', idade,'anos,' 'Sua Escolaridade é:', esc)
