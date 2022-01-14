5e1c7b71e1325bc7b64d1e970829f4aaeec3aacf
Roberto Nascimento <robertok3r@gmail.com>
2022-01-14 08:57:25 -0300

    Atualização 14-01-2022 as 08:57

---
+++ Aula 15 - Pass e Ellipsis como placeholders/aula15.py
@@ -0,0 +1,19 @@
+"""
+Pass e Ellipsis como placeholders
+
+"""
+'''
+valor = False
+if valor:  # vai dar erro
+    # comentario
+else:
+    print('tchau')
+    
+'''
+#corrigindo usando o pass
+valor = True
+if valor:
+    pass
+    #comentario
+else:
+    print('Tchau!!')
