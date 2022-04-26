5f4071c34c43ee102182169da5faaabbbbb7d82a
Roberto Nascimento <robertok3r@gmail.com>
2022-04-26 16:52:22 -0300

    26-04-2022 as 16:52

--- Seção 3 Python Intermediário -Programação Procedural/86 Módulos padrão do Python/Módulos padrão do Python.py
+++ Seção 3 Python Intermediário -Programação Procedural/86 Módulos padrão do Python/Módulos padrão do Python.py
@@ -4,3 +4,11 @@
 # Veja todos os módulos padrão do Python em:
 # https://docs.python.org/3/py-modindex.html
 
+import sys # informações do sistema
+import random # Gera numeros randomicos
+import os
+#print(sys.platform)
+#print(sys.version)
+#print(random.randint(0,10))
+for i in range(10):
+    print(random.randint(0,10))
